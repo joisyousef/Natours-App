8f10429ded66fbe61efecca44e173ee884ddbf38
joisyousef <yousefelsrogy500@gmail.com>
2024-12-06 18:05:39 +0200

    GET route to retrieve a single tour added

--- app.js
+++ app.js
@@ -19,7 +19,28 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+// GET route to retrieve a single tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
 
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 // POST route to add a new tour
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1; // Handle case when array is empty

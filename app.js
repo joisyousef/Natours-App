50b107d2c334ffaefb5a5546760c531f901b56de
joisyousef <yousefelsrogy500@gmail.com>
2024-12-08 14:00:25 +0200

    .env

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-
-app.use(morgan('dev'));
+console.olog(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development ') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello From the middleware');

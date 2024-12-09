2b9973add9d3902896c20d2dd95ffc6d23b984e7
joisyousef <yousefelsrogy500@gmail.com>
2024-12-09 17:14:29 +0200

    Connect the app with mongodb

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-console.olog(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development ') {
   app.use(morgan('dev'));
 }
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
+// Load environment variables
+dotenv.config({ path: './.env' });
 
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// Connect to MongoDB
+mongoose
+  .connect(DB)
+  // .connect(process.env.DATABASE_LOCAL)
+  .then(() => {
+    console.log('DB Connection Successful');
+  })
+  .catch((err) => {
+    console.error('DB Connection Error:', err);
+  });
+
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

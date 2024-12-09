97c68381f21e1d066e06e9b96b2760973ab249b2
joisyousef <yousefelsrogy500@gmail.com>
2024-12-09 19:25:46 +0200

    Fix .env expose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const { type } = require('superagent/lib/utils');
 
 // Load environment variables
 dotenv.config({ path: './.env' });
@@ -20,6 +21,24 @@ mongoose
   .catch((err) => {
     console.error('DB Connection Error:', err);
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // Start the server
 const port = process.env.PORT || 3000;

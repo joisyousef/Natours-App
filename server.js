a0665c7047d212efc268a543245c0faaca8aab1f
joisyousef <yousefelsrogy500@gmail.com>
2024-12-10 15:58:55 +0200

    Testing the database with test document

--- server.js
+++ server.js
@@ -40,6 +40,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
+  });
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

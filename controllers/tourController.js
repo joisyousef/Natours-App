43d16dd411c56835e49ffa9ce5c891f9b39a1ac1
joisyousef <yousefelsrogy500@gmail.com>
2024-12-10 20:38:35 +0200

    Testing models with postman

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-useless-path-segments
 const Tour = require('./../models/tourModel');
 
 // const tours = JSON.parse(
@@ -12,15 +13,16 @@ const Tour = require('./../models/tourModel');
 //     });
 //   next();
 // };     was for testing middleware functionality
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: 'Missing name or price in request body',
-    });
-  }
-  next();
-};
+
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'Fail',
+//       message: 'Missing name or price in request body',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -45,13 +47,23 @@ exports.getTour = (req, res) => {
   //   },
   // });
 };
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  // const newTours = new Tour({});
+  // newTours.save();
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(204).json({

c7256d7b3e162bb980019f9493169a269a33488d
joisyousef <yousefelsrogy500@gmail.com>
2025-03-13 06:10:41 +0200

    Handled Unhandled Routes

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // Middlewares
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development ') {
-  app.use(morgan('dev'));
-}
+// console.log(process.env.NODE_ENV);
+// if (process.env.NODE_ENV === 'development ') {
+//   app.use(morgan('dev'));
+// }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -27,4 +28,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on  this server!`,
+  });
+});
 module.exports = app;

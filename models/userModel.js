636ce540c8bd03fe9ff74d19c7c0238110c5abdc
joisyousef <yousefelsrogy500@gmail.com>
2025-06-20 00:13:32 +0300

    added auth to login and the method to check the password is correct or not

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -39,12 +40,22 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
   // Only hash the password if it has been modified (or is new)
   if (!this.isModified('password')) return next();
+
   // Hash the password with a cost factor of 12
   this.password = await bcrypt.hash(this.password, 12);
+
   // Delete passwordConfirm field after hashing
   this.passwordConfirm = undefined;
   next();
 });
+
+// Method to check if the provided password is correct
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;

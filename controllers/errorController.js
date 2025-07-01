670d8b9acd0c2e6a683db012fa9e30ac17d7c468
Yousef Elsrogy <yousefelsrogy500@gmail.com>
2025-07-01 12:47:35 +0300

    refactor: streamline user and tour controllers with factory functions and improve error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,49 +1,61 @@
-  const AppError = require('../utils/appError');
+const AppError = require('./../utils/appError');
 
-const handleCastErrorDB = (err) => {
+const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
-const handleJWTError = () =>
-  new AppError('Invalid Token. Please log in again!', 401);
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
 
-const handleJWTExpireError = () =>
-  new AppError('Your Token has expired! Please log in again.', 401);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
-    stack: err.stack,
+    stack: err.stack
   });
 };
-const handleDuplicateFieldDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
 
-  const message = `Duplicate Field Value: ${value}. Please Use Another Value!`;
-  return new AppError(message, 400);
-};
 const sendErrorProd = (err, res) => {
-  // Operational, Treusted error: send message to client
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message,
+      message: err.message
     });
-    // Proggramming or other unknown error: don't leak error details
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    // 1) Log Error
-    console.error('ERROR', err);
-    // 2) Send Generic Message
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'something went very wrong!',
+      message: 'Something went very wrong!'
     });
   }
 };
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
@@ -54,10 +66,14 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
-    if (error.name === 'TokenExpiredError') error = handleJWTExpireError();
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
     sendErrorProd(error, res);
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,25 +1,35 @@
 const express = require('express');
-const userController = require('../controllers/userController.js');
-const authController = require('../controllers/authController.js');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// Debug: Check if all functions exist
+console.log('userController functions:', Object.keys(userController));
+console.log('authController functions:', Object.keys(authController));
+
+// Check specific functions that might be undefined
+console.log('getAllUsers:', typeof userController.getAllUsers);
+console.log('protect:', typeof authController.protect);
+console.log('signup:', typeof authController.signup);
+
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword,
-);
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
-  .get(userController.getAllUsers)
+  // .get(userController.getAllUsers)
   .post(userController.createUser);
+
 router
   .route('/:id')
   .get(userController.getUser)
